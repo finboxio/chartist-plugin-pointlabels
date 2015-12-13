a08b7bb949543cdeefa58432c3bda2ffa9eb9c79
abpai <andy@finbox.io>
2015-12-12 18:58:41 -0800

    add support for bar chart labels; add option to hide x cordinate in point labels; add ability to provide formatting function to labels

--- src/scripts/chartist-plugin-pointlabels.js
+++ src/scripts/chartist-plugin-pointlabels.js
@@ -13,7 +13,9 @@
       y: -10
     },
     textAnchor: 'middle',
-    labelInterpolationFnc: Chartist.noop
+    labelInterpolationFnc: Chartist.noop,
+    labelFormat: Chartist.noop,
+    hideX: false
   };
 
   Chartist.plugins = Chartist.plugins || {};
@@ -22,14 +24,36 @@
     options = Chartist.extend({}, defaultOptions, options);
 
     return function ctPointLabels(chart) {
-      if(chart instanceof Chartist.Line) {
+      if(chart instanceof Chartist.Line || chart instanceof Chartist.Bar) {
         chart.on('draw', function(data) {
           if(data.type === 'point') {
             data.group.elem('text', {
               x: data.x + options.labelOffset.x,
               y: data.y + options.labelOffset.y,
               style: 'text-anchor: ' + options.textAnchor
-            }, options.labelClass).text(options.labelInterpolationFnc(data.value.x === undefined ? data.value.y : data.value.x + ', ' + data.value.y));
+            }, options.labelClass).text(options.labelInterpolationFnc((options.hideX || data.value.x === undefined) ? data.value.y : data.value.x + ', ' + data.value.y));
+          }
+        });
+      }
+
+      if(chart instanceof Chartist.Bar) {
+        chart.on('draw', function (data) {
+          var barHorizontalCenter, barVerticalCenter, label, value;
+          if (data.type === 'bar') {
+            barHorizontalCenter = data.x1 + (data.element.width() * .5);
+            barVerticalCenter = data.y1 + (data.element.height() * -1) - 10;
+            value = data.element.attr('ct:value');
+            if (value !== '0') {
+              label = new Chartist.Svg('text');
+              label.text(options.labelFormat(value));
+              label.addClass('ct-barlabel');
+              label.attr({
+                x: barHorizontalCenter,
+                y: barVerticalCenter,
+                'text-anchor': 'middle'
+              });
+              return data.group.append(label);
+            }
           }
         });
       }
